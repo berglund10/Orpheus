6ba561187660fe3baa8791052c4dae835eacdca7
Berglund Anton <anton.berglund@iths.se>
2024-09-12 13:40:57 +0200

    Added react + webpack

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Välkommen till Orpheus!</h1>
+            <div className="LoginContainer">
+                <h2>Logga in eller skapa ett konto för att börja.</h2>
+                <button>Logga in</button>
+                <button>Skapa konto</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;

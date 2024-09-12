e721b2d29d13fb003576d7c3852fdd2030214499
Berglund Anton <anton.berglund@iths.se>
2024-09-12 15:16:15 +0200

    Download react packages from CDN if in production mode

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,34 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
+
+module.exports = (env, argv) => {
+
+  const mode = argv.mode === "development" ? "development" : "production";
+
+  console.log("Mode is:", mode);
+
+  const externals = mode === "production" ? {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  } : {};
+
+  return {
+    mode: mode,
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader'],
-            },
-        ]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, '/src/index-' + mode + '.html')
+      })
     ],
     devServer: {
       static: './dist',
-      port:8080
+      port: 8080
     },
+    externals: externals,
+  }
 };

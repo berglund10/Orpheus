a3c4249a1f2f286093ebb516b77ea45bd0d2a5af
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-17 22:11:54 +0200

    Added logout to navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,49 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../App';
+import { useContext } from 'react';
 
 function NavBar() {
-  const navStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '10px 20px',
-    backgroundColor: '#333',
-    color: 'white'
-  };
+    const navigate = useNavigate();
+    const { user } = useContext(UserContext)
+    const handleLogout = () => {
+        if (user.auth) {
+            user.auth = false;
+            navigate('/');
+        }
+    };
 
-  const linkStyle = {
-    color: 'white',
-    textDecoration: 'none',
-    fontSize: '18px',
-  };
+    const navStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 20px',
+        backgroundColor: '#333',
+        color: 'white'
+    };
 
-  const ulStyle = {
-    listStyleType: 'none',
-    display: 'flex'
-  };
+    const linkStyle = {
+        color: 'white',
+        textDecoration: 'none',
+        fontSize: '18px',
+    };
 
-  const liStyle = {
-    marginRight: '20px'
-  };
+    const ulStyle = {
+        listStyleType: 'none',
+        display: 'flex'
+    };
 
-  return (
-    <nav style={navStyle}>
-      <div>
-        <h1>Orpheus</h1>
-      </div>
-      <ul style={ulStyle}>
-        <li style={liStyle}>
-          <Link to="/" style={linkStyle}>Home</Link>
-        </li>
-        <li style={liStyle}>
-          <Link to="/signup" style={linkStyle}>Sign up</Link>
-        </li>
-        <li style={liStyle}>
-        <div style={linkStyle}>Log out</div>
-        </li>
-      </ul>
-    </nav>
-  );
+    const liStyle = {
+        marginRight: '20px'
+    };
+
+    return (
+        <nav style={navStyle}>
+            <div>
+                <h1>Orpheus</h1>
+            </div>
+            <ul style={ulStyle}>
+                <li style={liStyle}>
+                    <Link to="/" style={linkStyle}>Home</Link>
+                </li>
+                <li style={liStyle}>
+                    <Link to="/signup" style={linkStyle}>Sign up</Link>
+                </li>
+                <li style={liStyle}>
+                    <div onClick={handleLogout} style={linkStyle}>Log out</div>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;

8837e414b88677115c2f1c922424a490b457ee93
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-17 23:02:25 +0200

    fixed imports

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,5 +1,4 @@
-import { useContext, useState, useEffect } from "react";
-import React from 'react';
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from "../App";
 

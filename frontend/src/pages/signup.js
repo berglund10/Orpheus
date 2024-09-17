b6cc6d803cde9eb46e867802334a3d49d0a5c1dc
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-17 23:02:11 +0200

    Added page for signup

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { useContext } from 'react';
-import { UserContext } from '../App';
+import React, { useState } from "react";
 
 function SignupPage() {
-  const {user} = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const createAccount = (e) => {
+    e.preventDefault();
+    if (true) {
+      console.log("Logik för att skapa konto här sen")
+    } else {
+      setErrorMessage("Kunde inte skapa användaren");
+    }
+  };
+
   return (
     <div>
-      <h2>Skapa ett nytt konto {user.username}</h2>
+      <h2>Skapa konto</h2>
+      <form onSubmit={createAccount}>
+        <div>
+          <label htmlFor="username">Användarnamn:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Lösenord:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        <button type="submit">Skapa konto</button>
+      </form>
     </div>
   );
 }

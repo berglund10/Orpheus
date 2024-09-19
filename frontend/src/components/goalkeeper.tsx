3f7d1b63b920a5914a8c609f71584efd79053e30
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-19 10:24:09 +0200

    get goalkeppers from football-API

--- frontend/src/components/goalkeeper.tsx
+++ frontend/src/components/goalkeeper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface GoalkeeperProps {
   name: string;
@@ -7,16 +7,48 @@ interface GoalkeeperProps {
 }
 
 const Goalkeeper = (props: GoalkeeperProps) => {
+  const [goalkeepers, setGoalkeepers] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://v3.football.api-sports.io/players/squads?team=496", {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": "secrect" // FREE KEY;
+        },
+      });
+      if (response.status === 200) {
+        const data = await response.json();
+        const playersArray = data.response[0].players;
+        const goalies = playersArray.filter((player: { position: string }) => player.position === 'Goalkeeper');
+        console.log(goalies)
+        setGoalkeepers(goalies);
+      }
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <>
       {props.name}
       <br />
-      {/* fetch(någonfotbollsapi)*/}
       <select name="goalkeepers">
-        <option value="Buffon">Buffon</option>
-        <option value="Cech">Cech</option>
-        <option value="Isaksson">Isaksson</option>
-        <option value="Szczesny">Szczesny</option>
+        {goalkeepers.length > 0 ? (
+          goalkeepers.map((goalkeeper) => (
+            <option key={goalkeeper.id} value={goalkeeper.name}>
+              {goalkeeper.name}
+            </option>
+          ))
+        ) : (
+          <option>Loading...</option>
+        )}
       </select>
     </>
   );

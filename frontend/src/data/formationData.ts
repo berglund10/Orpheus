d41986d14c515fed38d890b323418608b3d02a19
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-20 16:40:49 +0200

    Initial values for formation

--- frontend/src/data/formationData.ts
+++ frontend/src/data/formationData.ts
@@ -12,21 +12,21 @@ export interface Formation {
 }
 
 export const formation442: Formation = {
-  goalkeeper: { id: "1", name: "Michele Di Gregorio", position: "goalkeeper" },
+  goalkeeper: { id: "1", name: "GK", position: "goalkeeper" },
   defenders: [
-    { id: "2", name: "Pierre Kalulu", position: "defender" },
-    { id: "3", name: "Federico Gatti", position: "defender" },
-    { id: "4", name: "Bremer", position: "defender" },
-    { id: "5", name: "Andrea Cambiaso", position: "defender" },
+    { id: "2", name: "RB", position: "defender" },
+    { id: "3", name: "CB", position: "defender" },
+    { id: "4", name: "CB", position: "defender" },
+    { id: "5", name: "LB", position: "defender" },
   ],
   midfielders: [
-    { id: "6", name: "Manuel Locatelli", position: "midfielder" },
-    { id: "7", name: "Weston McKennie", position: "midfielder" },
-    { id: "8", name: "Nicolás González", position: "midfielder" },
-    { id: "9", name: "Teun Koopmeiners", position: "midfielder" },
+    { id: "6", name: "MF", position: "midfielder" },
+    { id: "7", name: "MF", position: "midfielder" },
+    { id: "8", name: "MF", position: "midfielder" },
+    { id: "9", name: "MF", position: "midfielder" },
   ],
   forwards: [
-    { id: "10", name: "Kenan Yildiz", position: "forward" },
-    { id: "11", name: "Dušan Vlahović", position: "forward" },
+    { id: "10", name: "CF", position: "forward" },
+    { id: "11", name: "CF", position: "forward" },
   ],
 };

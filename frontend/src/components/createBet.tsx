48e3403c7e3ee2d599bbe0cee464c8ae4494c9e5
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-20 17:15:02 +0200

    add createBet component

---
+++ frontend/src/components/createBet.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+
+function CreateBet() {
+    const [amount, setAmount] = useState("0");
+    const createBet = () => {
+        // http request to backend to make the bet
+        console.log(`Created a bet with the amount ${amount}` )
+    }
+
+
+    return(
+        <div>
+            <h1>Create bet here</h1>
+            <form onSubmit={createBet}>
+        <div>
+          <label htmlFor="bet">Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <br/>
+        <button type="submit">Submit</button>
+      </form>
+        </div>
+    )
+}
+
+export default CreateBet
--- frontend/src/pages/bets.tsx
+++ frontend/src/pages/bets.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AvailableBets from "../components/availableBets";
+import CreateBet from "../components/createBet";
 
 function Bets() {
   return (
     <div>
       <h1>Place bets</h1>
       <AvailableBets/>
+      <CreateBet/>
     </div>
   );
 }

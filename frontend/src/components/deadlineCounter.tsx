6634b69b7b0c46042a77e1c0f47457d1198dbc1a
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-21 11:03:59 +0200

    add deadline component

---
+++ frontend/src/components/deadlineCounter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function DeadlineCounter() {
+    const [timeleft, setTimeleft] = useState("10 hours");
+    // After choosing a league, calculate how much time left until the round starts.
+    // picking eleven closes 1 hour before.
+    return(
+        <div>
+            <h1>Hours until deadline: {timeleft}</h1>
+        </div>
+    )
+}
+
+export default DeadlineCounter

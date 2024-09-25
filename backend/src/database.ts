4d3c7b2c8fa0f5c4972737159762fda7b7abb81a
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-25 11:07:55 +0200

    forgot return

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -71,9 +71,10 @@ export default class Database {
     const user = result.rows[0];
     return user;
   }
-  async getUserByUsername(username: string) {
+  async getUserByUsername(username: string){
     const query = "SELECT * FROM users WHERE username = $1";
     const result = await this.getConnection().query(query, [username])
+    return result;
   }
 
   async getUsers(): Promise<User[]> {

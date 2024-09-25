4cb6a21549168f20d46bf8336f3ba079a5369a3d
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-25 10:23:43 +0200

    test for password as empty string

--- backend/src/test/password.test.ts
+++ backend/src/test/password.test.ts
@@ -3,14 +3,20 @@ import test from 'node:test';
 import { hashPassword, checkHashedPassword } from "../password";
 import assert from 'node:assert';
 
-test('testing hashPassword & chechHashedPassword functions', () => {
+test("testing hashPassword & chechHashedPassword functions", () => {
     const hashedPassword = hashPassword("12345");
     const isValid = checkHashedPassword("12345", hashedPassword);
     ok(isValid);
 })
 
-test('Password should not allow non-strings', () => {
+test("Password should not allow non-strings", () => {
   assert.throws(() => {
     hashPassword(12345 as any);
   }, new Error('password must be a string'));
 });
+
+test("Password can't be a empty string", () => {
+  assert.throws(() => {
+    hashPassword("" as any);
+  }, new Error("password can't be empty"))
+})

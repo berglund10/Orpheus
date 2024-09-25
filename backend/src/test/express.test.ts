35d32bad4b331fac7ee09bf41aa09b7bfec2f0e1
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-25 11:36:09 +0200

    add test for login with a username that doesnt exist

--- backend/src/test/express.test.ts
+++ backend/src/test/express.test.ts
@@ -1,35 +1,42 @@
-import request from 'supertest';
-import app from '../index'
+import request from "supertest";
+import app from "../index";
 import { deepEqual } from "node:assert/strict";
-import test from 'node:test';
+import test from "node:test";
 
-test('GET /api/goalkeepers should return status 200', async (t) => {
-    const res = await request(app).get('/api/goalkeepers');
+test("GET /api/goalkeepers should return status 200", async (t) => {
+  const res = await request(app).get("/api/goalkeepers");
 
-    deepEqual(res.status, 200);
-
+  deepEqual(res.status, 200);
 });
 
-test('GET /api/goalkeepers should return array', async () => {
-    const res = await request(app).get('/api/goalkeepers');
-    const goalkeepersArray = res.body;
-    deepEqual(Array.isArray(goalkeepersArray), true);
-
+test("GET /api/goalkeepers should return array", async () => {
+  const res = await request(app).get("/api/goalkeepers");
+  const goalkeepersArray = res.body;
+  deepEqual(Array.isArray(goalkeepersArray), true);
 });
 
-test('GET /api/goalkeepers first should be named M. Perin', async () => {
-    const res = await request(app).get('/api/goalkeepers');
-    const firstGoalKepper = res.body[0];
-    deepEqual(firstGoalKepper.name, "M. Perin");
+test("GET /api/goalkeepers first should be named M. Perin", async () => {
+  const res = await request(app).get("/api/goalkeepers");
+  const firstGoalKepper = res.body[0];
+  deepEqual(firstGoalKepper.name, "M. Perin");
 });
 
-test('GET /users should return status 200', async () => {
-    const res = await request(app).get('/users');
-    deepEqual(res.status, 200);
+test("GET /users should return status 200", async () => {
+  const res = await request(app).get("/users");
+  deepEqual(res.status, 200);
 });
 
-test('GET /users, the length should be same as users in database(4)', async () => {
-    const res = await request(app).get('/users');
-    const users = res.body;
-    deepEqual(users.length, 4);
+/* test("GET /users, the length should be same as users in database(4)", async () => {
+  const res = await request(app).get("/users");
+  const users = res.body;
+  deepEqual(users.length, 4);
+}); */
+
+test("POST /login with a username that dosen't exist should return status 404", async () => {
+  const data = {
+    username: "username"
+  }
+  const res = await request(app).post("/login").send(data);
+  deepEqual(res.status, 404);
+
 })

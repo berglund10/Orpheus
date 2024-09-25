1f880499ef8f73e5e459984d1b04ab9b8080273c
Anton Berglund <antonberglund_92@hotmail.com>
2024-09-25 10:18:32 +0200

    test for allow non-strings passwords

--- backend/src/test/password.test.ts
+++ backend/src/test/password.test.ts
@@ -1,9 +1,16 @@
 import { ok } from "node:assert/strict";
 import test from 'node:test';
 import { hashPassword, checkHashedPassword } from "../password";
+import assert from 'node:assert';
 
 test('testing hashPassword & chechHashedPassword functions', () => {
     const hashedPassword = hashPassword("12345");
     const isValid = checkHashedPassword("12345", hashedPassword);
     ok(isValid);
 })
+
+test('Password should not allow non-strings', () => {
+  assert.throws(() => {
+    hashPassword(12345 as any);
+  }, new Error('password must be a string'));
+});
